6a9d5c0f24100f5d6b2508864ceb7e266e3cd252
Samuel Chase <samebchase@gmail.com>
2013-02-15 20:17:10 +0530

    Able to print to *standard-output*, but not able to generate a string.

--- utils.lisp
+++ utils.lisp
@@ -1,6 +1,19 @@
 (in-package :millipode)
 
-(defparameter *content-path* 
+(defun list-files-with-extension (pathspec suffix)
+  (loop for pathname in (cl-fad:list-directory pathspec)
+       when (string= (pathname-type pathname) suffix) collect pathname))
 
-(defun hello ()
-  (format t "Millipode!"))
+(defun list-of-strings (pathspec)
+  (let ((string-list (cl-ppcre:split "\\n\\n" (alexandria:read-file-into-string pathspec))))
+    string-list))
+
+(defun test-html (string-list)
+  (cl-who:with-html-output (stream *standard-output* :indent t)
+    (cl-who:htm
+     (:body
+      (loop for string in string-list do
+	   (cl-who:htm (:p (cl-who:str string))))))))
+					   
+	   
+    
