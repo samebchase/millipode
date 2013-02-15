90d96313631c35ce43bd5c6b01e12fcf917c270a
Samuel Chase <samebchase@gmail.com>
2013-02-15 22:05:28 +0530

    Basic functionality for string-list->html.

--- utils.lisp
+++ utils.lisp
@@ -1,19 +1,42 @@
 (in-package :millipode)
 
+(defun list-files (pathspec)
+  (cl-fad:list-directory pathspec))
+
+(defun list-files-recursively (pathspec)
+  (let ((file-list ()))
+    (cl-fad:walk-directory pathspec (lambda (file) (push file file-list)))
+    file-list))   
+
 (defun list-files-with-extension (pathspec suffix)
-  (loop for pathname in (cl-fad:list-directory pathspec)
+  (loop for pathname in (list-files pathspec)
        when (string= (pathname-type pathname) suffix) collect pathname))
 
 (defun list-of-strings (pathspec)
   (let ((string-list (cl-ppcre:split "\\n\\n" (alexandria:read-file-into-string pathspec))))
     string-list))
 
-(defun test-html (string-list)
-  (cl-who:with-html-output (stream *standard-output* :indent t)
+(setf (cl-who:html-mode) :html5)
+
+(defmacro string-who (string-list)
+  `(loop for list in ,string-list
+	collect `(:p ,list)))
+
+(defun string-list->html (string-list)
+  (cl-who:with-html-output-to-string (*standard-output* nil :indent t :prologue t)
     (cl-who:htm
-     (:body
-      (loop for string in string-list do
-	   (cl-who:htm (:p (cl-who:str string))))))))
-					   
-	   
-    
+     (:head
+      (:link :href "../style/style.css" :rel "stylesheet" :type "text/css" :media "screen")
+      (:link :rel "icon" :type "image/png" :href "../images/favicon.png")
+      (:title (cl-who:esc (first string-list))))
+      (:body
+       (:article :class "post"
+	(:h3 (cl-who:esc (first string-list)))
+	(cl-who:htm
+	 (loop for string in (rest string-list) do
+	     (cl-who:htm (:p (cl-who:esc string))))))
+       (:footer "Here be the footer of the page")))))
+
+
+
+       
