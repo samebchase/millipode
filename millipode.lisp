84b80ccac51fe97d0584c79d048d89688a10135c
Samuel Chase <samebchase@gmail.com>
2013-02-16 17:17:44 +0530

    Removed unnecessary code and refactored generate-post out of generate-all-posts.

--- millipode.lisp
+++ millipode.lisp
@@ -3,14 +3,24 @@
 (defparameter *content-path* (cl-fad:pathname-as-directory "../blog/"))
 (defparameter *webpage-path* (cl-fad:pathname-as-directory "../pages/"))
 
-(defun generate-blog-posts (content-path webpage-path)
-  (let ((file-list (list-files-with-extension content-path "txt")))
+(assert (and (cl-fad:directory-exists-p *content-path*)
+	     (cl-fad:directory-exists-p *webpage-path*)))
+
+(defun generate-post (filepath webpage-path)
+  (format t "Generating html for: ~a.~%" filepath)
+  (alexandria:write-string-into-file
+   (gen-blog-post-html filepath)
+   (merge-pathnames webpage-path (pathname-name filepath))))
+
+(defun generate-all-posts (content-path webpage-path)
+  (let ((file-list (cl-fad:list-directory content-path)))
     (loop for filepath in file-list do
-	 (format t "Generating html for: ~a.~%" filepath)
-	 (alexandria:write-string-into-file (gen-blog-post-html filepath)
-					    (change-extension filepath webpage-path ".html")))))
+	 (generate-post filepath webpage-path))))
 
 (defun generate-index (webpage-path)
   (alexandria:write-string-into-file
    (generate-index-html webpage-path)
    (pathname (format nil "~a~a" *webpage-path* "index.html"))))
+
+(generate-all-posts *content-path* *webpage-path*)
+(generate-index *webpage-path*)
--- utils.lisp
+++ utils.lisp
@@ -3,11 +3,6 @@
 (defun list-files (pathspec)
   (cl-fad:list-directory pathspec))
 
-(defun list-files-recursively (pathspec)
-  (let ((file-list ()))
-    (cl-fad:walk-directory pathspec (lambda (file) (push file file-list)))
-    file-list))   
-
 (defun list-files-with-extension (pathspec suffix)
   (loop for pathname in (list-files pathspec)
        when (string= (pathname-type pathname) suffix) collect pathname))
@@ -16,8 +11,6 @@
   (let ((string-list (cl-ppcre:split "\\n\\n" (alexandria:read-file-into-string pathspec))))
     string-list))
 
-(defun change-extension (src-file dest-dir extension)
-  ;;(assert (cl-f:regular-file-exists-p src-file))
-  ;;(assert (osicat:directory-exists-p dest-dir))
-  (let ((file-name-wo-ext (pathname-name src-file)))
-    (pathname (format nil "~a~a~a" dest-dir file-name-wo-ext extension))))
+(defun path-str (pathspec)
+  "Returns pathname as string"
+  (format nil "~a" pathspec))
