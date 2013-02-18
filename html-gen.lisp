40187a0fc9a63a8bc320c10d94e5215ab2172664
Samuel Chase <samebchase@gmail.com>
2013-02-18 13:37:19 +0530

    Cloned styling of current site.

--- html-gen.lisp
+++ html-gen.lisp
@@ -12,14 +12,25 @@
 	(:link :rel "icon" :type "image/png" :href "../img/s.png")
 	(:title "index"))
        (:body
-	(:h3 "posts")
-	(cl-who:htm
-	 (loop for file in file-list 
-	    unless (string= (pathname-name file) "index") do
-	      (cl-who:htm (:a :href file (cl-who:esc (pathname-name file))) :br))))
-       (:footer
-	(:a :href "index" "index")
-	(:a :href "../about" "home")))))))	
+	(:div :id "container"
+
+	      (:div :id "header"
+		    (:ul (:li (:a :href "../about" "home"))))
+
+	      (:div :id "sidebar")
+
+	      (:div :id "content" :style "border-bottom: 0px; min-height: 500px"
+		    (:h3 "posts")
+		    (cl-who:htm
+		     (:div :id "index_links"
+			   (loop for file in file-list 
+			      unless (string= (pathname-name file) "index") do
+				(cl-who:htm (:p (:a :href file (cl-who:esc (pathname-name file)))))))))
+
+	      (:div :id "filler")
+
+	      (:div :id "footer"
+		    (:div :id "empty_box")))))))))
 
 (defun gen-blog-post-html (dir)
   (let ((string-list (list-of-strings dir)))
@@ -31,8 +42,23 @@
 	 (:link :rel "icon" :type "image/png" :href "../img/s.png")
 	 (:title (cl-who:esc (first string-list))))
 	(:body
-	 (:article :class "post"
-		   (:h3 (cl-who:esc (first string-list)))
-		   (cl-who:htm
-		    (loop for string in (rest string-list) do
-			 (cl-who:htm (:p (cl-who:esc string))))))))))))
+	 (:div :id "container"
+	       (:div :id "header"
+		    (:ul (:li (:a :href "../about" "home"))
+			 (:li (:a :href "index" "index"))))
+
+	      (:div :id "sidebar")
+
+	      (:div :id "content" :style "border-bottom: 0px; min-height: 500px"
+
+		    (:h3 (cl-who:esc (first string-list)))
+		    (cl-who:htm
+		     (loop for string in (rest string-list) do
+			  (cl-who:htm (:p (cl-who:esc string))))))
+
+	      (:div :id "filler")
+
+	      (:div :id "footer"))))))))
+
+
+	 
