d5fb6859fc69aebc506f9b1f572fe84c2d7ee8ba
Samuel Chase <samebchase@gmail.com>
2013-02-20 17:03:50 +0530

    (use-package :cl-fad) in utils.lisp

--- utils.lisp
+++ utils.lisp
@@ -1,5 +1,7 @@
 (in-package :millipode)
 
+(use-package :cl-fad)
+
 ;; TODO: figure out an elegant way of testing the existence of
 ;; multiple files. A with-existing macro?
 
@@ -7,7 +9,7 @@
 ;; function. Keyword args?
 
 (defun ls (dir)
-  (cl-fad:list-directory dir))
+  (list-directory dir))
 
 (defun ls-ext (dir suffix)
   "Lists files with extension."
@@ -17,8 +19,8 @@
 (defun list-modified-content (content-dir webpage-dir)
   "Lists the text files that are newer than their corresponding
 generated html files."
-  (assert (and (cl-fad:directory-exists-p content-dir)
-	       (cl-fad:directory-exists-p webpage-dir)))
+  (assert (and (directory-exists-p content-dir)
+	       (directory-exists-p webpage-dir)))
   (loop for file in (remove-if-not (alexandria:curry #'generated-webpage-p webpage-dir) (ls content-dir))
      when (content-post-newerp file webpage-dir 2)
        collect file))
@@ -27,7 +29,7 @@ generated html files."
 ;; could be \n
 
 (defun list-of-strings (pathspec separator)
-  (assert (and (cl-fad:file-exists-p pathspec) (not (cl-fad:directory-pathname-p pathspec))))
+  (assert (and (file-exists-p pathspec) (not (directory-pathname-p pathspec))))
   (let ((string-list (cl-ppcre:split separator (alexandria:read-file-into-string pathspec))))
     string-list))
 
@@ -40,27 +42,27 @@ generated html files."
 (defun webpage-file (post-text-file webpage-dir)
   "Returns the name correspoding generated html file for the given
 text-file regardless of whether it exists or not."
-  (assert (cl-fad:file-exists-p post-text-file))
+  (assert (file-exists-p post-text-file))
   (merge-pathnames webpage-dir
 		   (pathname (format nil "~a~a" (pathname-name post-text-file) ".html"))))
 
 (defun text-file (webpage-file content-dir)
   "Does the converse of webpage-file."
-  (assert (cl-fad:file-exists-p webpage-file))
+  (assert (file-exists-p webpage-file))
   (merge-pathnames content-dir
 		   (pathname (format nil "~a~a" (pathname-name webpage-file) ".txt"))))
 
 (defun file-mod-time-diff (file-a file-b)
   "Returns the difference in seconds of the last-modified time."
-  (assert (and (cl-fad:file-exists-p file-a)
-	       (cl-fad:file-exists-p file-b)))
+  (assert (and (file-exists-p file-a)
+	       (file-exists-p file-b)))
   (- (file-write-date file-a)
      (file-write-date file-b)))
 
 (defun generated-webpage-p (webpage-dir content-file)
   "Predicate that tests whether a text-file's corresponding webpage
 exists."
-  (cl-fad:file-exists-p (webpage-file content-file webpage-dir)))
+  (file-exists-p (webpage-file content-file webpage-dir)))
 
 (defun list-new-content (content-dir webpage-dir)
   (loop for file in (ls content-dir)
@@ -72,7 +74,7 @@ exists."
 corresponding file in content-dir."
  (let ((webpages (ls webpage-dir)))
    (loop for webpage in webpages unless
-	(or (cl-fad:file-exists-p (text-file webpage content-dir))
+	(or (file-exists-p (text-file webpage content-dir))
 	    (string= (pathname-name webpage) "index"))
       collect webpage)))
 
@@ -81,13 +83,13 @@ corresponding file in content-dir."
 
 (defun content-post-newerp (post-text-file webpage-dir delay)
   (let ((generated-webpage (webpage-file post-text-file webpage-dir)))
-    (assert (and (cl-fad:file-exists-p post-text-file)
-		 (cl-fad:file-exists-p generated-webpage)))
+    (assert (and (file-exists-p post-text-file)
+		 (file-exists-p generated-webpage)))
     (> (file-mod-time-diff post-text-file generated-webpage) delay)))
 
 (defun delete-files-in-dir (pathspec)
   "Deletes all regular files in directory."
-  (assert (cl-fad:directory-exists-p pathspec))
+  (assert (directory-exists-p pathspec))
   (loop for file in (ls pathspec) do
-       (when (not (cl-fad:directory-pathname-p file))
+       (when (not (directory-pathname-p file))
 	 (delete-file file))))
