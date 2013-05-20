21b7e4dccddc4604df9805e30fb9acc84b7e9fb8
Samuel Chase <samebchase@gmail.com>
2013-05-20 15:30:43 +0530

    Indentation cleanup

--- millipode.lisp
+++ millipode.lisp
@@ -1,21 +1,21 @@
 (in-package :millipode)
 
 (defclass pode ()
-    ((content-dir :accessor content-dir :initarg :content-dir)
-     (webpage-dir :accessor webpage-dir :initarg :webpage-dir)))
+  ((content-dir :accessor content-dir :initarg :content-dir)
+   (webpage-dir :accessor webpage-dir :initarg :webpage-dir)))
 
 (defparameter *blog-pode*
   (make-instance 'pode
-		 :content-dir (fad:pathname-as-directory #P"../content/")
-		 :webpage-dir (fad:pathname-as-directory #P"../p/")))					 
+				 :content-dir (fad:pathname-as-directory #P"/home/samuel/projects/samebchase.com/site/content/")
+				 :webpage-dir (fad:pathname-as-directory #P"/home/samuel/projects/samebchase.com/site/p/")))
 
 (defun status ()
   (let ((modified (list-modified-content  *blog-pode*))
-	(new      (list-new-content       *blog-pode*))
-	(orphaned (list-orphaned-webpages *blog-pode*)))
+		(new      (list-new-content       *blog-pode*))
+		(orphaned (list-orphaned-webpages *blog-pode*)))
     (flet ((print-list-files (string list)
-	     (unless (null list)
-	       (format t "~a: ~{~a~%~}" string list))))
+			 (unless (null list)
+			   (format t "~a: ~{~a~%~}" string list))))
       (print-list-files "modified" modified)
       (print-list-files "new" new)
       (print-list-files "orphaned" orphaned))))
