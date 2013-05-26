3478d66625d77fd4342a42a13b8dc55820e408ef
Samuel Chase <samebchase@gmail.com>
2013-05-27 01:50:07 +0530

    Move initialisation of *blog-pode* to config.lisp

--- config.lisp
+++ config.lisp
@@ -1,10 +1,17 @@
 (in-package :millipode)
 
 ;;;; Edit the pathspecs as appropriate
-;;;; Multiple 
 
 (defparameter *blog-content-dir*
   #P"/home/samuel/projects/samebchase.com/site/content/")
 
 (defparameter *blog-webpage-dir*
   #P"/home/samuel/projects/samebchase.com/site/p/")
+
+(defparameter *blog-pode*
+  (make-instance 'pode
+                 :content-dir
+				 (fad:pathname-as-directory *blog-content-dir*)
+                 :webpage-dir
+				 (fad:pathname-as-directory *blog-webpage-dir*)))
+
