187a1c4abbca50221ccf5830e78837c994e7831a
Samuel Chase <samebchase@gmail.com>
2013-05-27 01:50:19 +0530

    Add pode.lisp

---
+++ pode.lisp
@@ -0,0 +1,14 @@
+(in-package :millipode)
+
+(defclass pode ()
+  ((content-dir :accessor content-dir :initarg :content-dir)
+   (webpage-dir :accessor webpage-dir :initarg :webpage-dir))
+  (:documentation
+   "A pode is a simple compound type consisting of two pathspecs
+content-dir and webpage-dir. content-dir is the directory containing
+text files and webpage-dir is where the the html files should be
+generated."))
+
+(defmethod print-object ((pode pode) stream)
+  (format stream "Content directory: ~a~%Webpage directory: ~a"
+		  (content-dir pode) (webpage-dir pode)))
