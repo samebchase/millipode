faaae266cd68d50e11cefe1aed56f4c5665b7b95
Samuel Chase <samebchase@gmail.com>
2013-05-27 02:08:33 +0530

    Implement split-date-name-file-ext

--- utils.lisp
+++ utils.lisp
@@ -96,6 +96,15 @@ exists."
   (unless (null list)
     (format t "~a: ~{~a~%~}" string list)))
 
+(defun split-date-name-file-ext (pathspec)
+  "#P\"1984-04-15-post-name.html\" => \"1984-04-15\" \"post-name\" \"html\""
+  (let ((date-post-name (file-namestring pathspec))
+        (regex "^([0-9]{4}-[0-9]{2}-[0-9]{2})-(.+)\\.(.+)$"))
+    (ppcre:register-groups-bind
+        (date post-name file-ext)
+        (regex date-post-name)
+      (values date post-name file-ext))))
+
 (defun get-post-title (filespec)
   (flet ((get-first-line (filespec)
            (with-open-file (stream filespec)
