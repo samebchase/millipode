d2c46a9a4f3af11457e27e4c9bc12659bbde87c7
Samuel Chase <samebchase@gmail.com>
2013-05-28 01:38:18 +0530

    Change to constants

--- config.lisp
+++ config.lisp
@@ -2,15 +2,15 @@
 
 ;;;; Edit the pathspecs as appropriate
 
-(defparameter *blog-content-dir*
+(defconstant +blog-content-dir+
   #P"/home/samuel/projects/samebchase.com/site/content/")
 
-(defparameter *blog-webpage-dir*
+(defconstant +blog-webpage-dir+
   #P"/home/samuel/projects/samebchase.com/site/p/")
 
-(defparameter *blog-pode*
+(defconstant +blog-pode+
   (make-instance 'pode
                  :content-dir
-                 (fad:pathname-as-directory *blog-content-dir*)
+                 (fad:pathname-as-directory +blog-content-dir+)
                  :webpage-dir
-                 (fad:pathname-as-directory *blog-webpage-dir*)))
+                 (fad:pathname-as-directory +blog-webpage-dir+)))
