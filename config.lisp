29c482c1997b8f9d544a921967f04d91fc8d652d
Samuel Chase <samebchase@gmail.com>
2013-05-29 00:22:54 +0530

    Use alexandria:define-constant
    added pode-equal

--- config.lisp
+++ config.lisp
@@ -1,14 +1,16 @@
 (in-package :millipode)
 
-(defparameter *blog-content-dir*
-  #P"/home/samuel/projects/samebchase.com/site/content/")
+;;;; Edit the pathspecs as appropriate
 
-(defparameter *blog-webpage-dir*
-  #P"/home/samuel/projects/samebchase.com/site/p/")
+(alexandria:define-constant +blog-content-dir+
+  #P"/home/samuel/projects/samebchase.com/site/content/" :test #'fad:pathname-equal)
 
-(defparameter *blog-pode*
+(alexandria:define-constant +blog-webpage-dir+    
+  #P"/home/samuel/projects/samebchase.com/site/p/" :test #'fad:pathname-equal)
+
+(alexandria:define-constant +blog-pode+
   (make-instance 'pode
                  :content-dir
-                 (fad:pathname-as-directory *blog-content-dir*)
+                 (fad:pathname-as-directory +blog-content-dir+)
                  :webpage-dir
-                 (fad:pathname-as-directory *blog-webpage-dir*)))
+                 (fad:pathname-as-directory +blog-webpage-dir+)) :test #'pode-equal)
