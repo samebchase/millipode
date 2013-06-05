6e0faf2f90ddd3accac42867ef20afb648350854
Samuel Chase <samebchase@gmail.com>
2013-06-05 21:05:59 +0530

    add make-load-form to fix ccl build error

--- pode.lisp
+++ pode.lisp
@@ -10,8 +10,13 @@ text files and webpage-dir is where the the html files should be
 generated."))
 
 (defmethod print-object ((pode pode) stream)
-  (format stream "content: ~a~%webpages: ~a"
-          (content-dir pode) (webpage-dir pode)))
+  (print-unreadable-object (pode stream :type t :identity t)
+    (format stream "content: ~a~%webpages: ~a"
+            (content-dir pode) (webpage-dir pode))))
+
+(defmethod make-load-form ((pode pode) &optional env)
+  (declare (ignore env))
+  (make-load-form-saving-slots pode))
 
 (defun pode-equal (pode-a pode-b)
   (and (fad:pathname-equal (content-dir pode-a) (content-dir pode-b))
