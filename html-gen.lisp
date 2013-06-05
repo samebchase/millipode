f78b9b5f846979e741aeae5bcb77079d60ce7069
Samuel Chase <samebchase@gmail.com>
2013-06-06 00:05:18 +0530

    Refactor generate-post-index-html

--- html-gen.lisp
+++ html-gen.lisp
@@ -4,32 +4,31 @@
 
 (defun generate-post-index-html (pode)
   (with-existing-pode-slots pode
-    (flet ((index-page-p (webpage)
-             (string= (pathname-name webpage) "index")))
-      (let ((webpages (remove-if #'index-page-p (ls webpage-dir)))
-            (posts    (ls content-dir)))
-        (with-html-output-to-string (*standard-output* nil :indent t :prologue t)
-          (:html
-            (:head
-             (:link :href "../css/style.css" :rel "stylesheet" :type "text/css" :media "screen")
-             (:link :rel "icon" :type "image/png" :href "../img/s.png")
-             (:title "archive"))
-            (:body
-             (:div :id "container"
-                   (:div :id "header"
-                         (:ul (:li (:a :href "../index.html" "home"))))
-                   (:div :id "sidebar")
-                   (:div :id "content" :style "border-bottom: 0px; min-height: 300px" ;; TODO: inline-css ugh.
-                         (:h3 "archive")
-                         (:div :id "index_links"
-                               (loop for webpage in (reverse webpages) for post in (reverse posts)
-                                  do
-                                    (htm (:p
-                                          (:a :href (escape-string (file-namestring webpage))
-                                              (esc (get-post-title post))))))
-                               (:div :id "filler")
-                               (:div :id "footer"
-                                     (:div :id "empty_box"))))))))))))
+    (flet ((webpage (post)
+             (file-namestring (corresponding-webpage-file post webpage-dir))))
+    (let ((posts (ls content-dir)))
+      (with-html-output-to-string (*standard-output* nil :indent t :prologue t)
+        (:html
+          (:head
+           (:link :href "../css/style.css" :rel "stylesheet" :type "text/css" :media "screen")
+           (:link :rel "icon" :type "image/png" :href "../img/s.png")
+           (:title "archive"))
+          (:body
+           (:div :id "container"
+                 (:div :id "header"
+                       (:ul (:li (:a :href "../index.html" "home"))))
+                 (:div :id "sidebar")
+                 (:div :id "content" :style "border-bottom: 0px; min-height: 300px" ;; TODO: inline-css ugh.
+                       (:h3 "archive")
+                       (:div :id "index_links"
+                             (loop for post in (reverse posts) do
+                                  (htm (:p
+                                        (:a :href
+                                            (escape-string (webpage post))                                             
+                                            (esc (get-post-title post))))))
+                             (:div :id "filler")
+                             (:div :id "footer"
+                                   (:div :id "empty_box"))))))))))))
 
 (defun gen-blog-post-html (file)
   (check-type file pathname)
