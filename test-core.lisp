b54215f530ff564f0b56794d0453eb9eb5d0d764
Samuel Chase <samebchase@gmail.com>
2013-06-12 03:44:37 +0530

    remove test-core.lisp, to-do.org; update .gitignore

--- test-core.lisp
+++
@@ -1,8 +0,0 @@
-(asdf:load-system :millipode)
-
-(in-package :millipode)
-
-#+sbcl    (sb-ext:save-lisp-and-die +image-file-name+ :toplevel          'cli :executable t)
-#+clisp   (ext:saveinitmem          +image-file-name+ :init-function     'cli :executable t :quiet t)
-#+clozure (ccl:save-application     +image-file-name+ :toplevel-function 'cli :prepend-kernel t)
-
