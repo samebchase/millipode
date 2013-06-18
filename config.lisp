5ef6f5b0c55e6961a16841d2a4aab49a2dad5678
Samuel Chase <samebchase@gmail.com>
2013-06-19 01:58:37 +0530

    Add +index-template-file+ and +post-template-file+

--- config.lisp
+++ config.lisp
@@ -1,15 +1,14 @@
 (in-package :millipode)
 
-;;;; Main configuration Edit the pathspecs as appropriate
+;;;; Edit the pathspecs as appropriate.
+;;;; Directories must have a trailing slash.
 
 (define-constant +blog-content-dir+
-    #P"/home/samuel/projects/samebchase.com/site/content/" :test #'fad:pathname-equal)
-
+    #P"/home/samuel/projects/samebchase.com/site/content/" :test #'pathname-equal)
 (define-constant +blog-webpage-dir+
-    #P"/home/samuel/projects/samebchase.com/site/p/" :test #'fad:pathname-equal)
-
+    #P"/home/samuel/projects/samebchase.com/site/p/" :test #'pathname-equal)
 (define-constant +blog-template-dir+
-    #P"/home/samuel/projects/samebchase.com/site/templates/" :test #'fad:pathname-equal)
+    #P"/home/samuel/projects/samebchase.com/site/templates/" :test #'pathname-equal)
 
 (define-constant +blog-pode+
   (make-instance 'pode
@@ -20,6 +19,13 @@
                  :template-dir
                  (pathname-as-directory +blog-template-dir+))
   :test #'pode-equal)
+
+;;;; Template file configuration
+
+(define-constant +index-template-file+
+    #P"post-index.template.html" :test #'pathname-equal)
+(define-constant +post-template-file+
+    #P"post.template.html"       :test #'pathname-equal)
 
 ;;;; The name of the executable program
 
--- html-gen.lisp
+++ html-gen.lisp
@@ -1,10 +1,11 @@
 (in-package :millipode)
 
 (defun generate-post-index-html (pode)
+  "The post index HTML returned as a string."
   (with-existing-pode-slots pode
     (flet ((webpage (post)
              (file-namestring (corresponding-webpage-file pode post))))
-      (let* ((template-file (merge-pathnames #P"post-index.template.html" template-dir))
+      (let* ((template-file (merge-pathnames +index-template-file+ template-dir))
              (posts   (ls content-dir))
              (sstream (make-string-output-stream))
              (values  (list :posts
@@ -15,16 +16,17 @@
         (get-output-stream-string sstream)))))
 
 (defun markdown-to-html (file)
+  (check-type file pathname)
   (let ((sstream (make-string-output-stream)))
     (parse-string-and-print-to-stream
      (read-file-into-string file) sstream)
     (get-output-stream-string sstream)))
 
 (defun gen-blog-post-html (pode file)
-  "A post's HTML generated as a string."
+  "A post's HTML returned as a string."
   (check-type file pathname)
   (with-existing-pode-slots pode
-    (let* ((template-file (merge-pathnames #P"post.template.html" template-dir))
+    (let* ((template-file (merge-pathnames +post-template-file+ template-dir))
            (sstream (make-string-output-stream))
            (values (list :title (get-post-title file)
                          :post  (markdown-to-html file)))
